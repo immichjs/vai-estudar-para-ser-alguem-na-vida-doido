9f98c52f39b49eebc802b1ca2b51a58f4c94791a
Michel França <fr.mich17@gmail.com>
2023-10-31 05:58:34 -0300

    projeto finalizado

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script setup></script>
+
+<template>
+  <main
+    class="flex flex-col gap-20 justify-center items-center bg-gray-900 h-screen"
+  >
+    <div class="text-white">
+      <h1 class="font-medium text-6xl">Vá estudar imediatamente!</h1>
+      <p class="text-center mt-4">Você ainda é pobre.</p>
+    </div>
+
+    <ul class="flex gap-24">
+      <li class="flex items-end">
+        <a
+          href="https://www.linkedin.com/jobs/search/?keywords=desenvolvedor&location=Brasil"
+          target="_blank"
+        >
+          <img src="./assets/img/logo-linkedin.png" class="w-40" alt="" />
+        </a>
+      </li>
+      <li class="flex items-end">
+        <a href="https://udemy.com" target="_blank">
+          <img src="./assets/img/logo-udemy.svg" class="w-40" alt="" />
+        </a>
+      </li>
+      <li class="flex items-end">
+        <a href="https://www.alura.com.br/" target="_blank">
+          <img src="./assets/img/logo-alura.svg" class="w-40" alt="" />
+        </a>
+      </li>
+      <li class="flex items-end">
+        <a href="https://dio.me" target="_blank">
+          <img src="./assets/img/logo-dio.svg" class="w-40" alt="" />
+        </a>
+      </li>
+    </ul>
+  </main>
+</template>
+
+<style scoped></style>
